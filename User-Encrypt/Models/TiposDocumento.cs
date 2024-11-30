41fc7d49514b4cbef94cf1a4a43faacb7949ed2c
alejandra08-lang <rodriguezaleja304@gmail.com>
2024-11-30 09:38:17 -0500

    Se realizo el modelo TiposDocumento

---
+++ User-Encrypt/Models/TiposDocumento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace User_Encrypt.Models
+{
+    public class TiposDocumento
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(2, MinimumLength = 4, ErrorMessage = "Este campo debe tener entre 2 a 4 caracteres")]
+        public string Title { get; set; }
+    }
+}
